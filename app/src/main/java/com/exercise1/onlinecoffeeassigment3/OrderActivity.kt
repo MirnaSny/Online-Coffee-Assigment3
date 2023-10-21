23a8f1e5bd0f02993b606787266d0705159e8c51
MirnaSny <116234495+MirnaSny@users.noreply.github.com>
2023-10-21 21:26:28 +0300

    change the name of the bar

--- app/src/main/java/com/exercise1/onlinecoffeeassigment3/OrderActivity.kt
+++ app/src/main/java/com/exercise1/onlinecoffeeassigment3/OrderActivity.kt
@@ -7,5 +7,6 @@ class OrderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order)
+
     }
 }
