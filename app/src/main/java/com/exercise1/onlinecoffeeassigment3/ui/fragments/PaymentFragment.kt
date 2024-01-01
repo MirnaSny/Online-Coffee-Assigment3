6f677a90c6d4798e9607980141e254597221e78b
MirnaSny <116234495+MirnaSny@users.noreply.github.com>
2024-01-01 13:48:06 +0300

    add fun validation in paymentFragment class

--- app/src/main/java/com/exercise1/onlinecoffeeassigment3/ui/fragments/PaymentFragment.kt
+++ app/src/main/java/com/exercise1/onlinecoffeeassigment3/ui/fragments/PaymentFragment.kt
@@ -19,4 +19,35 @@ class PaymentFragment : Fragment(){
         binding = FragmentPaymentBinding.inflate(layoutInflater)
         return binding.root
     }
+
+
+    private fun setupUI(){
+
+    }
+
+    private fun showSpinner(){
+        binding.btnPlaceOrderOne.setOnClickListener{
+
+        }
+    }
+    private fun isValidateNameAndNumber():Boolean{
+    val isVal:Boolean
+    if (binding.editFullName.length() == 0){// it means the user still not give any data
+        binding.editFullName.error = "Please Enter Your Name" //it means give the error to user
+
+    }else{
+        binding.editFullName.error = null
+
+    }
+        if (binding.editPhoneNumber.length() != 10){
+            binding.editPhoneNumber.error = "Failed Phone Number"
+            isVal = false
+        }else{
+        binding.editPhoneNumber.error= null
+            isVal=true
+    }
+        return isVal
+    }
+
+    //validation = to make sure the user add the number and name in the app
 }
