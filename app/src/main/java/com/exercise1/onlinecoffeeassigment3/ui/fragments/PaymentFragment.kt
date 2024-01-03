a32fff8172354da0b6b05b7941b1c18afd357d37
MirnaSny <116234495+MirnaSny@users.noreply.github.com>
2024-01-03 16:44:09 +0300

    add new fun about showCardNumberAndExpiry , change in xml and make it more clean

--- app/src/main/java/com/exercise1/onlinecoffeeassigment3/ui/fragments/PaymentFragment.kt
+++ app/src/main/java/com/exercise1/onlinecoffeeassigment3/ui/fragments/PaymentFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import com.exercise1.onlinecoffeeassigment3.R
 import com.exercise1.onlinecoffeeassigment3.databinding.FragmentPaymentBinding
 
-class PaymentFragment : Fragment(){
+class PaymentFragment : Fragment() {
 
     private lateinit var binding: FragmentPaymentBinding
 
@@ -20,34 +23,90 @@ class PaymentFragment : Fragment(){
         return binding.root
     }
 
-
-    private fun setupUI(){
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupUI()
+        setupListener()
     }
 
-    private fun showSpinner(){
-        binding.btnPlaceOrderOne.setOnClickListener{
+    private fun setupListener() {
+        binding.continueBtn3.setOnClickListener{
 
         }
     }
-    private fun isValidateNameAndNumber():Boolean{
-    val isVal:Boolean
-    if (binding.editFullName.length() == 0){// it means the user still not give any data
-        binding.editFullName.error = "Please Enter Your Name" //it means give the error to user
 
-    }else{
-        binding.editFullName.error = null
 
+    private fun setupUI() {
+        showSpinner()
+        setSpinnerAdapter()
+        showCardNumberAndExpiry()
     }
-        if (binding.editPhoneNumber.length() != 10){
+
+
+    private fun showSpinner() {
+        binding.btnPlaceOrderOne.setOnClickListener {
+            if (isValidateNameAndNumber()) {
+                binding.clSpinner.visibility = View.VISIBLE
+                binding.btnPlaceOrderOne.visibility = View.GONE
+            }
+        }
+    }
+
+    private fun isValidateNameAndNumber(): Boolean {
+        //validation = to make sure the user add the number and name in the app
+
+        val isVal: Boolean
+        if (binding.editFullName.length() == 0) {// it means the user still not give any data
+            binding.editFullName.error = "Please Enter Your Name" //it means give the error to user
+
+        } else {
+            binding.editFullName.error = null
+
+        }
+        if (binding.editPhoneNumber.length() != 10) {
             binding.editPhoneNumber.error = "Failed Phone Number"
             isVal = false
-        }else{
-        binding.editPhoneNumber.error= null
-            isVal=true
-    }
+        } else {
+            binding.editPhoneNumber.error = null
+            isVal = true
+        }
         return isVal
     }
 
-    //validation = to make sure the user add the number and name in the app
+    private fun setSpinnerAdapter() {
+        //create adapter for spinner -> arrayCard
+        ArrayAdapter.createFromResource(
+            binding.spinnerCardType.context,
+            R.array.CardType,
+            android.R.layout.simple_spinner_item
+
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.spinnerCardType.adapter = adapter
+        }
+    }
+
+    private fun showCardNumberAndExpiry() {
+        binding.spinnerCardType.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                // selectedItem -> it will give me the position that the user selected
+                val selectedItemPosition =parent?.getItemAtPosition(position)
+                cardVisibility(selectedItemPosition.toString())
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                cardVisibility(null)
+            }
+        }
+    }
+
+    //cardVisibility -> I control the visibility of the card number and expiry
+    private fun cardVisibility(selectedItem:String?){
+        binding.cardNumberExpiry.visibility = if (selectedItem == "_") View.GONE else View.VISIBLE
+        binding.btnPlaceOrder2.visibility=if (selectedItem == "_") View.VISIBLE else View.GONE
+        binding.continueBtn3.visibility = if (selectedItem == "_") View.GONE else View.VISIBLE
+
+    }
+
+
+
 }
