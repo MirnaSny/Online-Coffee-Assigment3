495b6bcb722006afa3905f409989136d623beeee
MirnaSny <116234495+MirnaSny@users.noreply.github.com>
2024-02-15 20:39:30 +0300

    edit in summaryAdapter

--- app/src/main/java/com/exercise1/onlinecoffeeassigment3/domain/ItemsViewModel.kt
+++
@@ -1,4 +0,0 @@
-package com.exercise1.onlinecoffeeassigment3.domain
-
-data class ItemsViewModel(val title :String , val content:String)
-// title : order , content -> "american coffee " + size + type
